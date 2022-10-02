a0e5e577bcfebd5b54232102ae09242d7e91f805
noman <nomanplacidenoman@gmail.com>
2022-10-02 12:55:46 +0100

    F02 feat Add Route of diffent page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Basket from "./pages/Basket";
+import Contact from "./pages/Contact";
+import Connexion from "./pages/Connexion";
 
 function App() {
   return (
     <Routes>
       <Route>
         <Route path="/" element={<Home />} exact />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/connexion" element={<Connexion />} />
+        <Route path="/signin" element={<Signin />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/basket" element={<Basket />}/>
       </Route>
     </Routes>
   );
